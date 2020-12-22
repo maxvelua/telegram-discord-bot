1b15dc02f445279a19896b997572533a5ea7def3
Volodymyr Karpinskyi <bnmdude@gmail.com>
2020-12-22 01:05:37 +0100

    refactor bot replays to ukrainian language

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const discordController = require('./controllers/discord.controller');
 const telegramService = require('./services/telegram.service');
 const express = require('express');
 
-if (!process.env.TELEGRAM_BOT_API_KEY) {
-    console.log('Add your telegram bot api key to .env file');
+if (!process.env.TELEGRAM_BOT_API_KEY ||
+    !process.env.DISCORD_INVITE_URL ||
+    !process.env.TELEGRAM_USERNAME) {
+    console.log('Pls check api keys in .env file');
     return;
 }
 
@@ -14,11 +16,18 @@ const client = new Telegraf(process.env.TELEGRAM_BOT_API_KEY);
 const app = express();
 
 // telegram side
-client.start((ctx) => ctx.reply('Welcome'));
-client.help((ctx) => ctx.reply('Send me a sticker'));
+client.start((ctx) => ctx.reply('Просто відправ мені мемас, а решту роботи я зроблю за тебе.'));
+
+client.help((ctx) => ctx.reply(
+    'Відправляй мені мемаси, а я перекину їх в дс.' +
+    '\nНаразі бот працює тільки з одним сервером.' +
+    `\n${process.env.DISCORD_INVITE_URL}` +
+    `\n @${process.env.TELEGRAM_USERNAME}`)
+);
+
 client.on('message', async (ctx) => {
     const type = ctx.updateSubTypes[0];
-
+    console.log(ctx);
     if (type === 'animation' ||
         (type === 'sticker' && !ctx.update.message.sticker.is_animated) ||
         type === 'photo' ||
@@ -31,10 +40,11 @@ client.on('message', async (ctx) => {
             const response = await discordController.sendContent(JSON.stringify(content));
             ctx.reply(response);
         } else {
-            ctx.reply('Your meme is too big, pls boost your server❗');
+            ctx.reply('Ваш мем занадто великий, будь ласка, прискорте сервер❗');
         }
     } else {
-        ctx.reply('This meme type doesn\'t support yet❗');
+        ctx.reply('Це тип мемів ще  не підтримується❗' +
+            '\n\\meme щоб дізнатись більше про можливості бота.');
     }
 });
 
